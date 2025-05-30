c5a25784074e026ae9203544be14563969c9e79c
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-05-30 16:41:49 +0530

    g-tag

---
+++ home/admin.py
@@ -0,0 +1,84 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here. 1
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['id','logo','logo_2', 'color']
+admin.site.register(Setting,SettingAdmin)
+
+
+# Register your models here. 2
+
+class Web_SliderAdmin(admin.ModelAdmin):
+    list_display = ['id','web_image','mobile_image', 'title']
+admin.site.register(Web_Slider,Web_SliderAdmin)
+
+
+# Register your models here. 3
+class OverviewAdmin(admin.ModelAdmin):
+    list_display = ['id','web_image','mobile_image', 'title','details']
+admin.site.register(Overview,OverviewAdmin)
+
+
+
+# Register your models here. 4
+class About_UsAdmin(admin.ModelAdmin):
+    list_display = ['id','title','details']
+admin.site.register(About_Us, About_UsAdmin)
+
+
+# Register your models here. 5
+class Unique_Selling_PropositionAdmin(admin.ModelAdmin):
+    list_display = ['id','icone','title',]
+admin.site.register(Unique_Selling_Proposition,Unique_Selling_PropositionAdmin)
+
+
+# Register your models here. 6
+class ConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['id','typology','rera_carpet_area','price']
+admin.site.register(Configuration, ConfigurationAdmin)
+
+
+# Register your models here. 7
+class AmenitiesAdmin(admin.ModelAdmin):
+    list_display = ['id','icone','title',]
+admin.site.register(Amenities, AmenitiesAdmin)
+
+
+# Register your models here. 8
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['id','web_image','title']
+admin.site.register(Gallery, GalleryAdmin)
+
+
+
+
+# Register your models here. 8
+class BookingopenAdmin(admin.ModelAdmin):
+    list_display = ['id','project_name','at', 'by', 'landp_arcel','possession','spot_booking_offers','early_buy_discounts','flexipay_for_first','luxurious','priceing']
+admin.site.register(Bookingopen, BookingopenAdmin)
+
+
+# Register your models here. 8
+class WelcometoAdmin(admin.ModelAdmin):
+    list_display = ['id','title','details', 'readmore',]
+admin.site.register(Welcometo, WelcometoAdmin)
+
+# Register your models here. 8
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id','title',]
+admin.site.register(Location, LocationAdmin)
+
+
+class MahareraAdmin(admin.ModelAdmin):
+    list_display = ['id','title','maharera_no','details']
+admin.site.register(Maharera, MahareraAdmin)
+
+class ReraaditionalAdmin(admin.ModelAdmin):
+    list_display = ['id','project_registered','Government_RERA_Authorised_Advertiser','RERA_Project_Registration_No','Site_Address','Contact_Us','Disclaimer']
+admin.site.register(Reraaditional, ReraaditionalAdmin)
+
+
+class Why_InvestAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discripation']
+admin.site.register(Why_Invest, Why_InvestAdmin)
---
+++ home/models.py
@@ -0,0 +1,194 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+# Create your models here.
+from django.forms import ModelForm, TextInput, Textarea
+from django.http import request
+from django.utils.safestring import mark_safe
+
+
+
+
+# Create your models here.
+
+class Setting(models.Model):
+    title = models.CharField(max_length=150,)
+    website_link = models.CharField(max_length=150,)
+    keywords = models.CharField(max_length=255,)
+    description = models.CharField(max_length=255,)
+    configuration_bg = models.ImageField(upload_to='logo/')
+    logo = models.ImageField(upload_to='logo/',)
+    logo_2 = models.ImageField(upload_to='logo/')
+    icon = models.ImageField(upload_to='images/',)
+    virtual_site_visit = models.ImageField(upload_to='images/',)
+    color = models.CharField(max_length=150)
+    cuntact_no = models.CharField(max_length=150,)
+    location = models.CharField(max_length=1150,)
+    site_address= models.CharField(max_length=1150,blank=True,null=True)
+    googletagmanager= models.CharField(max_length=1150,blank=True,null=True)
+    privacy_policy = RichTextField(blank=True,null=True)
+    
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='9. Header'
+
+class Web_Slider(models.Model):
+    web_image = models.ImageField(upload_to='sliderimage/')
+    mobile_image = models.ImageField(upload_to='sliderimage/')
+    title = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='2. Web Slider'
+
+class Overview(models.Model):
+    web_image = models.ImageField(upload_to='overviewimage/')
+    mobile_image = models.ImageField(upload_to='overview/')
+    title = models.CharField(max_length=150)
+    details= models.TextField(blank=False,max_length=5500)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='3. Overview'
+
+class About_Us(models.Model):
+    title = models.CharField(max_length=150)
+    details= models.TextField(blank=False,max_length=5500)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='4. About_Us'
+
+class Unique_Selling_Proposition(models.Model):
+    icone = models.ImageField(upload_to='uspimage/')
+    title = models.CharField(max_length=150)
+    icone2 = models.ImageField(upload_to='uspimage/',blank=True,null=True)
+    title2 = models.CharField(max_length=150,blank=True,null=True)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='5. Unique Selling Proposition'
+
+class Configuration(models.Model):
+    typology = models.CharField(max_length=150)
+    rera_carpet_area = models.CharField(max_length=150)
+    price = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.typology    
+        
+    class Meta:
+        verbose_name_plural='6. Configuration'
+
+class Amenities(models.Model):
+    icone = models.ImageField(upload_to='amenitiesimage/')
+    title = models.CharField(max_length=150)
+    icone2 = models.ImageField(upload_to='uspimage/',blank=True,null=True)
+    title2 = models.CharField(max_length=150,blank=True,null=True)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='7. Amenities'
+
+class Gallery(models.Model):   
+    web_image = models.ImageField(upload_to='galleryimage/')
+    title = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='8. Gallery'
+     
+
+class Bookingopen(models.Model):
+    project_name = models.CharField(max_length=150,)
+    at = models.CharField(max_length=255,)
+    by = models.CharField(max_length=255,)
+    landp_arcel = models.CharField(max_length=255,)
+    floors = models.CharField(max_length=255,)
+    possession = models.CharField(max_length=255,)
+    spot_booking_offers = models.CharField(max_length=255,)
+    early_buy_discounts = models.CharField(max_length=255,)
+    flexipay_for_first = models.CharField(max_length=255,)
+    luxurious = models.CharField(max_length=255,)
+    priceing = models.CharField(max_length=255,)
+    
+    
+    def __str__(self):
+        return self.project_name    
+        
+    class Meta:
+        verbose_name_plural='10. Booking Open'
+
+class Welcometo(models.Model):
+    web_image = models.ImageField(upload_to='aboutimage/')
+    title = models.CharField(max_length=150)
+    details= models.TextField(blank=False,max_length=5500)
+    readmore= models.TextField(blank=False,max_length=5500)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='11. Welcome To'
+
+class Location(models.Model):
+    title = models.CharField(max_length=150)    
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='12. Location'
+
+class Maharera(models.Model):
+    qr_image = models.ImageField(upload_to='overviewimage/')
+    title = models.CharField(max_length=150)
+    maharera_no= models.CharField(blank=True,max_length=50)
+    details= models.CharField(blank=True,max_length=500)
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='13. Maharera'
+
+class Reraaditional(models.Model):
+    project_registered = models.CharField(max_length=350)
+    Government_RERA_Authorised_Advertiser = models.CharField(max_length=500)
+    RERA_Project_Registration_No = models.CharField(max_length=500)
+    Site_Address = models.CharField(max_length=350)
+    Contact_Us = models.CharField(max_length=500)
+    Disclaimer = models.CharField(max_length=1500)
+    
+    def __str__(self):
+        return self.project_registered    
+        
+    class Meta:
+        verbose_name_plural='14. RERA Aditional'
+  
+class Why_Invest(models.Model):
+    title = models.CharField(max_length=350)
+    discripation = models.CharField(max_length=500)
+    
+    
+    def __str__(self):
+        return self.title    
+        
+    class Meta:
+        verbose_name_plural='15. Why_Invest'
