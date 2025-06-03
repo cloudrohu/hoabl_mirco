43911d4600f2e29aeb29410df7828be9f9097b21
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-06-03 13:21:59 +0530

    id

--- response/admin.py
+++ response/admin.py
@@ -4,5 +4,5 @@ from .models import *
 # Register your models here.
 
 class ResponseAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email', 'phone','created_at','updated_at',)
+    list_display = ('name','email', 'phone','created_at','updated_at',)
 admin.site.register(Response, ResponseAdmin)
